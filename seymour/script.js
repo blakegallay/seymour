28f37b855a7cdbb3a0f084154a7b9208a561e054
hockeymonday <adamakadhim@gmail.com>
2020-06-24 20:56:53 -0400

    skeleton

---
+++ seymour/script.js
@@ -0,0 +1,15 @@
+findTargetText(document.body)
+
+/* A recursive function to highlight a given word on a webpage. */
+function findTargetText(element) {
+    // If the element has children nodes, recurse.
+    if (element.hasChildNodes()) {
+        element.hasChildNodes.forEach(findTargetText)
+        // Otherwise see if it is a text element and use regex to find word.
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        // This is where we highlight the word.
+        if (element.textContent === /WORD/gi) {
+            element.style.backgroundColor = "#FDFF47"
+        }
+    }
+}
